b244d6bf9b498cfcdb118ca096c45fb0e8d4531d
bss9395 <bss9395@users.noreply.github.com>
2018-04-18 21:47:13 +0800

    none

---
+++ _zh/Mathematics/Combinatorics/Important_Patterns.md
@@ -0,0 +1,12 @@
+---
+layout:    zh_post
+Topic:     组合数学
+Title:     重要图案
+Revised:   2018-04-18 02:59:00 +08 @ 中国-新疆-乌鲁木齐 +06
+Authors:   璀璨星辰
+Resources:
+---
+
+> ### 二项式三角形
+
+> ![max-width:360px;](figures/Binomial_Triangle.svg)
--- _zh/Mathematics/Probabilistic_Statistics/figures/sources/Gamma_Function.m
+++ _zh/Mathematics/Probabilistic_Statistics/figures/sources/Gamma_Function.m
@@ -17,7 +17,8 @@ plot([0, 2], [1, 1], 'k--');
 patch([-5, 5], [0, 0], 'black'); patch([5, 5 - 0.2, 5 - 0.2], [0, 0.1, -0.1], 'black');
 patch([0, 0], [-5, 5], 'black'); patch([0, 0 - 0.1, 0 + 0.1], [5, 5 - 0.2, 5 - 0.2], 'black');
 patch([-4:4; -4:4], [zeros(1, 9); 0.1 * ones(1, 9)], 'black');
-text([-4:4], [zeros(1, 9)], {'-4', '-3', '-2', '-1', '0', '1', '2', '3', '4'}, 'verticalalign', 'top', 'horizontalalign', 'center');
+patch([1.5, 1.5], [0, 0.1], 'black');
+text([-4:-1, 1:4], [zeros(1, 8)], {'-4', '-3', '-2', '-1', '1', '2', '3', '4'}, 'verticalalign', 'top', 'horizontalalign', 'center');
 patch([zeros(1, 9); 0.1 * ones(1, 9)], [-4:4; -4:4], 'black');
 text([zeros(1, 8)], [-4:-1, 1:4], {'-4 ', '-3 ', '-2 ', '-1 ', '1 ', '2 ', '3 ', '4 '}, 'verticalalign', 'middle', 'horizontalalign', 'right');
 
