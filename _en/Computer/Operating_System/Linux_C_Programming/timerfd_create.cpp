991a9ad872dbf8666492f0c2deca6ea72eba66b8
bss9395 <brilliantstarrysky9395@gmail.com>
2019-12-26 14:56:35 +0800

    none

--- _en/Computer/Operating_System/Linux_C_Programming/timerfd_create.cpp
+++ _en/Computer/Operating_System/Linux_C_Programming/timerfd_create.cpp
@@ -8,39 +8,36 @@ int timerfd_gettime(int fd, struct itimerspec *curr_value);
 #include <sys/timerfd.h>
 #include <unistd.h>
 
+#include <stdio.h>
+#include <stdbool.h>
 #include <errno.h>
-#include <iostream>
+#include <stdlib.h>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
-
-int checkError(bool failed, const string &file, const long line, const string &func, const string &msg) {
-  if(failed) {
-    cout << "==" << file << "==" << line << "==" << func << "==[" << errno << "]" << msg << endl;
-    if(errno) {
-      exit(errno);
-    }
-  }
-  return 0;
+int checkError(bool failed, const char *file, const long line, const char *function, const char *message) {
+	if (failed) {
+		fprintf(stderr, "\33[33m""%s==%ld==%s==[%d]%s""\33[0m\n", file, line, function, errno, message);
+		if (errno) {
+			exit(errno);
+		}
+	}
+	return 0;
 }
 
 int main(int argc, char *argv[]) {
-  int fd = timerfd_create(CLOCK_MONOTONIC, 0);
-  checkError(fd < 0, __FILE__, __LINE__, __FUNCTION__, "CLOCK_MONOTONIC");
+	int fd = timerfd_create(CLOCK_MONOTONIC, 0);
+	checkError(fd < 0, __FILE__, __LINE__, __FUNCTION__, "CLOCK_MONOTONIC");
 
-  struct itimerspec timeout = { {1, 0}, {1, 0} };
-  int err = timerfd_settime(fd, 0, &timeout, NULL);
-  checkError(err < 0, __FILE__, __LINE__, __FUNCTION__, "timerfd_settime");
+	struct itimerspec timeout = { {1, 0}, {1, 0} };
+	int err = timerfd_settime(fd, 0, &timeout, NULL);
+	checkError(err < 0, __FILE__, __LINE__, __FUNCTION__, "timerfd_settime");
 
-  long tick = 0;
-  while(true) {
-    long len = read(fd, &tick, sizeof(tick));
-    checkError(len != sizeof(tick), __FILE__, __LINE__, __FUNCTION__, "read");
-    cout << tick << endl;
-  }
+	long tick = 0;
+	while (true) {
+		long len = read(fd, &tick, sizeof(tick));
+		checkError(len != sizeof(tick), __FILE__, __LINE__, __FUNCTION__, "read");
+		fprintf(stdout, "%ld\n", tick);
+	}
 
-  close(fd);
-  return 0;
+	close(fd);
+	return 0;
 }
