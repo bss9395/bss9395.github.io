6a778a7fee8fb15594eb8ee6cb43616680f8b52b
bss9395 <brilliantstarrysky9395@gmail.com>
2020-05-10 17:00:31 +0800

    none

--- _en/Computer/Design_Pattern/Default/default.c
+++ _en/Computer/Design_Pattern/Default/default.c
@@ -3,30 +3,30 @@
 
 #define DEFAULT(value, fallback)  (#value[0] ? value + 0 : fallback)
 
-
-#define FUNC(d) \
-    func(DEFAULT(d, 123.0))
-void func(double d) {
-	printf("%lf\n", d);
-}
-
-
-#define FUNC2(d1, d2) \
-    func2(DEFAULT(d1, 12.0), DEFAULT(d2, 23.0))
-void func2(double d1, double d2) {
+#define FUNC(d1, d2) \
+    func(DEFAULT(d1, 12.0), DEFAULT(d2, 23.0))
+void func(double d1, double d2) {
 	printf("%lf, %lf\n", d1, d2);
 }
 
 
-int main(int argc, char* argv[]) {
-	FUNC();
-	FUNC(12.23);
+void Default(double val, char *def) {
+	char del = ':';
+	if (def != NULL) {
+		del = *def;
+	}
 
-	FUNC2();
-	FUNC2(, );
-	FUNC2(2.0, );
-	FUNC2(, 3.0);
-	FUNC2(2.0, 3.0);
+	printf("%lf, %c\n", val, del);
+}
+
+
+int main(int argc, char* argv[]) {
+	FUNC(, );
+	FUNC(2.0, );
+	FUNC(, 3.0);
+	FUNC(2.0, 3.0);
+
+	Default(12.23, NULL);
 
 	return 0;
 }
