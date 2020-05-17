31997baff18a7299854e2dd91d86b02547b406cf
bss9395 <brilliantstarrysky9395@gmail.com>
2020-05-17 17:27:39 +0800

    none

--- _en/Computer/Data_Structure/List/DoublyList.cpp
+++ _en/Computer/Data_Structure/List/DoublyList.cpp
@@ -1,13 +1,31 @@
 /* DoublyList.cpp
 Author: BSS9395
-Update: 2020-05-15T00:00:00+08@China-Guangdong-Zhanjiang+08
+Update: 2020-05-17T17:27:00+08@China-Guangdong-Zhanjiang+08
 Design: Doubly Linked List
+*/
+
+/*
+ 0   1   2   3   4   5   6
+ +---+---+---+---+---+---+
+ | D | o | u | b | l | y |
+ +---+---+---+---+---+---+
+   1   2   3   4   5   6
+  -6  -5  -4  -3  -2  -1
+
+
+ 0   1   2   3   4   5   6
+ +---+---+---+---+---+---+
+ | P | y | t | h | o | n |
+ +---+---+---+---+---+---+
+ 0   1   2   3   4   5   6
+-6  -5  -4  -3  -2  -1   0
 */
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 
+using std::cin;
 using std::cout;
 using std::cerr;
 using std::endl;
@@ -15,14 +33,23 @@ using std::ostream;
 using std::string;
 
 typedef long iptr;
+typedef const char *Level;
+static const struct {
+	Level _Info;
+	Level _ToDo;
+	Level _Warn;
+	Level _Error;
+	Level _Fatal;
+} ELevel = { "Info", "ToDo", "Warn", "Error", "Fatal" };
 
-
-bool Checkout(const bool &failed, const string &file, const iptr &line, const string &function, const iptr &error, const string &report) {
+bool Checkout(const bool &failed, const string &file, const iptr &line, const string &function, const Level &level, const string &report) {
+	// cerr << __FUNCTION__ << endl;
 	if (failed) {
-		cerr << "[" << file << ":" << line << ":" << function << "]" << error << "#" << report;
-		if (!(errno == 0 && error == 0)) {
-			cerr << "#" << strerror(errno);
-			throw error;
+		cerr << "[" << file << "==" << line << "==" << function << "]" << endl;
+		cerr << "##" << level << "##" << report;
+		if (!(errno == 0 && level == ELevel._Info)) {
+			cerr << "##" << strerror(errno);
+			throw level;
 		}
 		cerr << endl;
 	}
@@ -35,22 +62,23 @@ template<typename Type>
 class Node {
 	friend bool operator<(const Node &lhs, const Node &rhs) {
 		// cerr << __FUNCTION__ << endl;
-
 		return (lhs._data < rhs._data);
 	}
 
 	friend bool operator==(const Node &lhs, const Node &rhs) {
+		// cerr << __FUNCTION__ << endl;
 		return (lhs._data == rhs._data);
 	}
 
 	friend ostream &operator<<(ostream &os, const Node &node) {
+		// cerr << __FUNCTION__ << endl;
 		os << node._data;
 		return os;
 	}
 
 public:
-	Node(const Type &data = Type())
-		: _data(data), _next(nullptr), _prev(nullptr) {
+	Node(const Type &data = Type(), Node *prev = nullptr, Node *next = nullptr)
+		: _data(data), _prev(prev), _next(next) {
 		// cerr << __FUNCTION__ << endl;
 	}
 
@@ -67,29 +95,34 @@ public:
 
 template<typename Type>
 class DoublyList {
+	class DoublyListPtr;
+
+public:
 	typedef class Node<Type> Node;
 	typedef iptr(*Visit)(Node *node);
+	typedef class DoublyListPtr Iterator;
 
 public:
 	static iptr Print(Node *node) {
+		// cerr << __FUNCTION__ << endl;
 		iptr ret = 0;
+
 		cout << " => " << *node;
+
 		ret += 1;
 		return ret;
 	}
 
 public:
 	DoublyList()
-		:_size(0) {
+		: _size(0) {
 		// cerr << __FUNCTION__ << endl;
-
 		_head._next = &_head;
 		_head._prev = &_head;
 	}
 
 	~DoublyList() {
 		// cerr << __FUNCTION__ << endl;
-
 		iptr size = 0;
 		Node *curr = _head._next;
 		Node *node = nullptr;
@@ -101,7 +134,7 @@ public:
 			size += 1;
 		}
 
-		Checkout(_size != size, __FILE__, __LINE__, __FUNCTION__, errno, "_size != size");
+		Checkout(_size != size, __FILE__, __LINE__, __FUNCTION__, ELevel._Fatal, "_size != size");
 	}
 
 public:
@@ -120,56 +153,145 @@ public:
 		}
 		cout << endl;
 
-		Checkout(_size != ret, __FILE__, __LINE__, __FUNCTION__, errno, "_size != size");
+		Checkout(_size != ret, __FILE__, __LINE__, __FUNCTION__, ELevel._Fatal, "_size != size");
 		return ret;
 	}
 
 public:
 	iptr Attach(Node *node, iptr index = 0) {
-		// cerr << __FUNCTION__ << endl;
-
 		iptr ret = 0;
-		Node *curr = &_head;
 
+		Node *edge = &_head;
+		index = index % (_size + 1);
 		while (index > 0) {
-			curr = curr->_next;
+			edge = edge->_next;
 			index -= 1;
 		}
 		while (index < 0) {
-			curr = curr->_prev;
+			edge = edge->_prev;
 			index += 1;
 		}
 
-		node->_next = curr->_next;
-		curr->_next->_prev = node;
-		curr->_next = node;
-		node->_prev = curr;
-
+		node->_next = edge->_next;
+		edge->_next->_prev = node;
+		edge->_next = node;
+		node->_prev = edge;
 		_size += 1;
+
 		ret += 1;
 		return ret;
 	}
 
-	iptr Detach(Node **node, iptr index = 0) {
+	iptr Detach(Node **node, iptr index = 1) {
 		iptr ret = 0;
-		Node *curr = &_head;
 
-		while (index > 0) {
-			curr = curr->_next;
-			index -= 1;
+		if (!Checkout(_size <= 0 || index == 0, __FILE__, __LINE__, __FUNCTION__, ELevel._Info, "_size <= 0 || index == 0")) {
+			Node *knot = &_head;
+			index = (index > 0) ? ((index - 1) % _size + 1) : ((index + 1) % _size - 1);
+			while (index > 0) {
+				knot = knot->_next;
+				index -= 1;
+			}
+			while (index < 0) {
+				knot = knot->_prev;
+				index += 1;
+			}
+
+			*node = knot;
+			knot->_prev->_next = knot->_next;
+			knot->_next->_prev = knot->_prev;
+
+			_size -= 1;
+			ret += 1;
 		}
-		while (index < 0) {
-			curr = curr->_prev;
-			index += 1;
-		}
-
-		node = &(curr->_next);
-		curr->_next = curr->_next;
-		curr->_next->_prev = curr;
-
-		_size -= 1;
-		ret += 1;
 		return ret;
+	}
+
+
+public:
+	class DoublyListPtr {
+		friend bool operator==(const DoublyListPtr &lhs, const DoublyListPtr &rhs) {
+			// cerr << __FUNCTION__ << endl;
+			return (lhs._ptr == rhs._ptr);
+		}
+
+		friend bool operator!=(const DoublyListPtr &lhs, const DoublyListPtr &rhs) {
+			// cerr << __FUNCTION__ << endl;
+			return (lhs._ptr != rhs._ptr);
+		}
+
+	public:
+		DoublyListPtr(Node &head, Node *ptr = nullptr)
+			: _head(head), _ptr(ptr) {
+			// cerr << __FUNCTION__ << endl;
+		}
+
+	public:
+		DoublyListPtr &operator+=(int offset) {
+			// cerr << __FUNCTION__ << endl;
+			while (offset > 0) {
+				if (_ptr == &_head) {
+					_ptr = _ptr->_next;
+				}
+				_ptr = _ptr->_next;
+				offset -= 1;
+			}
+			while (offset < 0) {
+				if (_ptr == &_head) {
+					_ptr = _ptr->_prev;
+				}
+				_ptr = _ptr->_prev;
+				offset += 1;
+			}
+
+			return *this;
+		}
+
+		DoublyListPtr &operator-=(int offset) {
+			// cerr << __FUNCTION__ << endl;
+			while (offset > 0) {
+				if (_ptr = &_head) {
+					_ptr = _ptr->_prev;
+				}
+				_ptr = _ptr->_prev;
+				offset -= 1;
+			}
+			while (offset < 0) {
+				if (_ptr = &_head) {
+					_ptr = _ptr->_next;
+				}
+				_ptr = _ptr->_next;
+				offset += 1;
+			}
+
+			return *this;
+		}
+
+		Type &operator*() {
+			// cerr << __FUNCTION__ << endl;
+
+			return _ptr->_data;
+		}
+
+		Type *operator->() {
+			// cerr << __FUNCTION__ << endl;
+
+			return &(operator*());
+		}
+
+	public:
+		Node &_head;
+		Node *_ptr;
+	};
+
+	Iterator Beg() {
+		// cerr << __FUNCTION__ << endl;
+		return Iterator(_head, _head._next);
+	}
+
+	Iterator End() {
+		// cerr << __FUNCTION__ << endl;
+		return Iterator(_head, &_head);
 	}
 
 public:
@@ -185,11 +307,25 @@ int main(int argc, char *argv[]) {
 
 	Node<string> *node1 = new Node<string>("node1");
 	Node<string> *node2 = new Node<string>("node2");
+	Node<string> *node3 = new Node<string>("node3");
 
 	list.Attach(node1);
 	list.Attach(node2, -1);
+	list.Attach(node3, -1);
+
+	Node<string> *node;
+	list.Detach(&node, 2);
 
 	list.Traverse(DoublyList<string>::Print);
+
+	////////////////////////////////////////
+
+	DoublyList<string>::Iterator beg = list.Beg();
+	DoublyList<string>::Iterator end = list.End();
+	while (beg != end) {
+		cerr << " => " << *beg;
+		beg += 1;
+	}
 
 	return 0;
 }
