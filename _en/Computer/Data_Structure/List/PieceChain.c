25114658da69d8e4bf6b0e11349f2d6c8b980c1e
bss9395 <brilliantstarrysky9395@gmail.com>
2020-10-21 16:08:06 +0800

    none

--- _en/Computer/Data_Structure/List/PieceChain.c
+++ _en/Computer/Data_Structure/List/PieceChain.c
@@ -8,41 +8,44 @@ Design: Memory Control
 #include <stdlib.h>
 
 typedef struct _Piece {
-	struct _Piece *_prev;
+	struct _Piece *_link;
 	long _value;
 } Piece;
 
 #define PiecesInBlock 3
 typedef struct _Block {
 	struct _Block *_next;
-	Piece _piece[PiecesInBlock];
+	struct _Piece _piece[PiecesInBlock];
 } Block;
 
-static Block *head_block = NULL;
-static Piece *free_piece = NULL;
+
+static Block *_Head_Block = NULL;
+static Piece *_Free_Piece = NULL;
 
 static Piece *Exploit() {
 	Block *block = (Block *)calloc(1, sizeof(Block));
-	block->_next = head_block;
-	head_block = block;
+	block->_next = _Head_Block;
+	_Head_Block = block;
 
-	Piece *end = &block->_piece[PiecesInBlock];
 	Piece *beg = &block->_piece[0];
-	while (end -= 1, beg < end) {
-		end->_prev = end - 1;
+	Piece *end = &block->_piece[PiecesInBlock - 1];
+	while (beg < end) {
+		beg->_link = beg + 1;
+		beg += 1;
 	}
-	beg->_prev = NULL;
+	end->_link = _Free_Piece;
+	// end->_link = NULL;
 
-	return &block->_piece[PiecesInBlock - 1];
+	return &block->_piece[0];
 }
 
 static Piece *Insert(long value) {
-	if (free_piece == NULL) {
-		free_piece = Exploit();
+	if (_Free_Piece == NULL) {
+		_Free_Piece = Exploit();
 	}
 
-	Piece *piece = free_piece;
-	free_piece = piece->_prev;
+	Piece *piece = _Free_Piece;
+	_Free_Piece = piece->_link;
 	piece->_value = value;
 
 	return piece;
@@ -51,30 +54,29 @@ static Piece *Insert(long value) {
 static void Delete(Piece *piece) {
 	piece->_value = 9395;
 
-	piece->_prev = free_piece;
-	free_piece = piece;
+	piece->_link = _Free_Piece;
+	_Free_Piece = piece;
 }
 
-
 void Test() {
-	Piece *piece12 = Insert(12);
-	Piece *piece11 = Insert(11);
 	Piece *piece10 = Insert(10);
+	Piece *piece11 = Insert(11);
+	Piece *piece12 = Insert(12);
 
-	Piece *piece22 = Insert(22);
 	Delete(piece11);
-	Delete(piece10);
-	piece10 = Insert(110);
-	piece11 = Insert(111);
-	Piece *piece21 = Insert(21);
-	Piece *piece20 = Insert(20);
+	Delete(piece12);
+	piece12 = Insert(120);
+	piece11 = Insert(110);
 
-	while (head_block != NULL) {
+	Piece *piece20 = Insert(20);
+	Piece *piece21 = Insert(21);
+
+	while (_Head_Block != NULL) {
 		for (int i = 0; i < PiecesInBlock; i += 1) {
-			fprintf(stdout, "%ld\t", head_block->_piece[i]._value);
+			fprintf(stdout, "%ld\t", _Head_Block->_piece[i]._value);
 		}
 		fprintf(stdout, "\n");
-		head_block = head_block->_next;
+		_Head_Block = _Head_Block->_next;
 	}
 
 	return;
@@ -85,4 +87,3 @@ int main(int argc, char *argv[]) {
 
 	return 0;
 }
-
