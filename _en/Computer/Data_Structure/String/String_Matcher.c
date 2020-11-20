8d366621c16b06428478e79eb5fcef80a5ccd233
bss9395 <brilliantstarrysky9395@gmail.com>
2020-11-21 05:50:54 +0800

    none

--- _en/Computer/Data_Structure/String/String_Matcher.c
+++ _en/Computer/Data_Structure/String/String_Matcher.c
@@ -1,6 +1,6 @@
 /* String_Matcher.c
 Author: BSS9395
-Update: 2020-11-20T07:01:00+08@China-Guangdong-Zhanjiang+08
+Update: 2020-11-21T05:50:00+08@China-Guangdong-Zhanjiang+08
 Design: Pattern Matcher
 */
 
@@ -53,20 +53,70 @@ char *Match_SubString_Classic(char *str, long len_str, char *sub, long len_sub) 
 	if (Check(str == NULL || len_str < 0 || sub == NULL || len_sub < 0, ELevel._Error, __FUNCTION__, "str == NULL || len_str < 0 || sub == NULL || len_sub < 0", NULL)) {
 		exit(EXIT_FAILURE);
 	}
+	if (len_str < len_sub) {
+		return NULL;
+	}
 
 	long idx = 0;
 	char *end_str = str + len_str - len_sub + 1;
-	while (idx < len_sub && str < end_str) {
-		if (str[idx] == sub[idx]) {
-			idx += 1;
-		}
-		else {
-			str += 1;
-			idx = 0;
-		}
+	while (str < end_str && idx < len_sub) {
+		(str[idx] == sub[idx]) ? (idx += 1) : (str += 1, idx = 0);
 	}
 	return (idx >= len_sub) ? str : NULL;
 }
+
+char *Match_SubString_Hash_Classic(char *str, long len_str, char *sub, long len_sub) {
+	if (Check(str == NULL || len_str < 0 || sub == NULL || len_sub < 0, ELevel._Error, __FUNCTION__, "str == NULL || len_str < 0 || sub == NULL || len_sub < 0", NULL)) {
+		exit(EXIT_FAILURE);
+	}
+	if (len_str < len_sub) {
+		return NULL;
+	}
+
+	if (sizeof(ui64) <= len_sub) {
+		typedef ui64 Hash;
+		len_sub -= sizeof(Hash);
+		char *end_str = str + len_str - sizeof(Hash) + 1;
+		for (Hash hash = *(Hash *)sub; str < end_str; str += 1) {
+			if (*(Hash *)str == hash) {
+				long idx = 1;
+				for (; idx < len_sub && *(Hash *)(str + idx) == *(Hash *)(sub + idx); idx += 1);
+				if (idx >= len_sub) {
+					return str;
+				}
+			}
+		}
+	}
+	else if (sizeof(ui32) <= len_sub) {
+		typedef ui32 Hash;
+		len_sub -= sizeof(Hash);
+		char *end_str = str + len_str - sizeof(Hash) + 1;
+		for (Hash hash = *(Hash *)sub; str < end_str; str += 1) {
+			if (*(Hash *)str == hash) {
+				long idx = 1;
+				for (; idx < len_sub && *(Hash *)(str + idx) == *(Hash *)(sub + idx); idx += 1);
+				if (idx >= len_sub) {
+					return str;
+				}
+			}
+		}
+	}
+	else {
+		char *end_str = str + len_str - len_sub + 1;
+		for (; str < end_str; str += 1) {
+			if (str[0] == sub[0]) {
+				long idx = 1;
+				for (; idx < len_sub && str[idx] == sub[idx]; idx += 1);
+				if (idx >= len_sub) {
+					return str;
+				}
+			}
+		}
+	}
+	return NULL;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 /* String moves forward all the way up, but Pattern will rollback once unmatch.
 Si+0,Si+1,...,Si+n-1
@@ -94,9 +144,12 @@ sub:   b  i  g  b  i  g  g  i  r  l
 back: -1  0  0  0  1  2  3  0  0  0
 back: -1  0  0 -1  0  0  3  0  0  0
 */
-long *Generate_Rollback_Index_Classical(char *sub, long len, long back[]) {
-	if (Check(sub == NULL || len < 0 || back == NULL, ELevel._Error, __FUNCTION__, "str == NULL || len < 0 || back == NULL", NULL)) {
+long *Generate_Rollback_Index_Classical(char *sub, long back[], long len) {
+	if (Check(sub == NULL || len < 0, ELevel._Error, __FUNCTION__, "str == NULL || len < 0", NULL)) {
 		exit(EXIT_FAILURE);
+	}
+	if (back == NULL) {
+		back = (long *)calloc(len, sizeof(long));
 	}
 
 	long idx_str = 0;
@@ -121,9 +174,12 @@ sub:   b  i  g  b  i  g  g  i  r  l
 back: -1  0  0  0  1  2  3  0  0  0
 back: -1  0  0 -1  0  0  3  0  0  0
 */
-long *Generate_Rollback_Index_Classic(char *sub, long len, long back[]) {
-	if (Check(sub == NULL || len < 0 || back == NULL, ELevel._Error, __FUNCTION__, "str == NULL || len < 0 || back == NULL", NULL)) {
+long *Generate_Rollback_Index_Classic(char *sub, long back[], long len) {
+	if (Check(sub == NULL || len < 0, ELevel._Error, __FUNCTION__, "str == NULL || len < 0", NULL)) {
 		exit(EXIT_FAILURE);
+	}
+	if (back == NULL) {
+		back = (long *)calloc(len, sizeof(long));
 	}
 
 	long idx_str = 0;
@@ -146,9 +202,12 @@ sub:   b  i  g  b  i  g  g  i  r  l
 back: -1  0  0  0  1  2  3  0  0  0
 back: -1  0  0 -1  0  0  3  0  0  0
 */
-long *Generate_Rollback_Index(char *sub, long len, long back[]) {
-	if (Check(sub == NULL || len < 0 || back == NULL, ELevel._Error, __FUNCTION__, "str == NULL || len < 0 || back == NULL", NULL)) {
+long *Generate_Rollback_Index(char *sub, long back[], long len) {
+	if (Check(sub == NULL || len < 0, ELevel._Error, __FUNCTION__, "str == NULL || len < 0", NULL)) {
 		exit(EXIT_FAILURE);
+	}
+	if (back == NULL) {
+		back = (long *)calloc(len, sizeof(long));
 	}
 
 	long idx_str = 0;
@@ -165,15 +224,12 @@ long *Generate_Rollback_Index(char *sub, long len, long back[]) {
 	return back;
 }
 
-char *Match_SubString(char *str, long len_str, char *sub, long len_sub, long back[], bool bare) {
-	bool heap = false;
-	if (back == NULL) {
-		heap = true;
-		back = (long *)calloc(len_sub, sizeof(long));
-		Generate_Rollback_Index(sub, len_sub, back);
+char *Match_SubString(char *str, long len_str, char *sub, long back[], long len_sub) {
+	if (Check(str == NULL || len_str < 0 || sub == NULL || back == NULL || len_sub < 0, ELevel._Error, __FUNCTION__, "str == NULL || len_str < 0 || sub == NULL || back == NULL || len_sub < 0", NULL)) {
+		exit(EXIT_FAILURE);
 	}
-	else if (bare) {
-		Generate_Rollback_Index(sub, len_sub, back);
+	if (len_str < len_sub) {
+		return NULL;
 	}
 
 	char *end_str = str + len_str;
@@ -184,10 +240,6 @@ char *Match_SubString(char *str, long len_str, char *sub, long len_sub, long bac
 		}
 		str += 1;
 		idx_sub += 1;
-	}
-
-	if (heap) {
-		free(back);
 	}
 	return (idx_sub >= len_sub) ? (str - len_sub) : NULL;
 }
@@ -204,14 +256,23 @@ void Test_Match_SubString_Classic() {
 	fprintf(stdout, "%d, %s\n", match - str, match);
 }
 
+void Test_Match_SubString_Hash_Classic() {
+	char *str = "Iamabigbiggirl,inabigbigworld.";
+	char *sub = "bigbiggirl";
+	long len_str = Length(str);
+	long len_sub = Length(sub);
+	char *match = Match_SubString_Hash_Classic(str, len_str, sub, len_sub);
+	fprintf(stdout, "%s\n", match);
+}
+
 void Test_Rollback_Index() {
 	char *sub = "bigbiggirl";
 	long len = Length(sub);
 	long back[100];
 
-	Generate_Rollback_Index_Classical(sub, len, back);
-	// Generate_Rollback_Index_Classic(sub, len, back);
-	// Generate_Rollback_Index(sub, len, back);
+	Generate_Rollback_Index_Classical(sub, back, len);
+	// Generate_Rollback_Index_Classic(sub, back, len);
+	// Generate_Rollback_Index(sub, back, len);
 	for (long i = 0; i < len; i += 1) {
 		fprintf(stdout, "%ld, ", back[i]);
 	}
@@ -219,17 +280,19 @@ void Test_Rollback_Index() {
 }
 
 void Test_Match_SubString() {
-	char *str = "Iamabigbiggirlinabigbigworld";
+	char *str = "Iamabigbiggirl,inabigbigworld.";
 	char *sub = "bigbiggirl";
 	long len_str = Length(str);
 	long len_sub = Length(sub);
-
-	char *match = Match_SubString(str, len_str, sub, len_sub, NULL, true);
+	long *back = Generate_Rollback_Index(sub, NULL, len_sub);
+	char *match = Match_SubString(str, len_str, sub, back, len_sub);
 	fprintf(stdout, "%ld, %s\n", match - str, match);
+	free(back);
 }
 
 int main(int argc, char *argv[]) {
 	// Test_Match_SubString_Classic();
+	// Test_Match_SubString_Hash_Classic
 	Test_Rollback_Index();
 	Test_Match_SubString();
 
