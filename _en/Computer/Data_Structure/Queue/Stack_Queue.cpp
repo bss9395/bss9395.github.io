09a17bab6adb9632b84530c043c36c952cfcfab8
bss9395 <brilliantstarrysky9395@gmail.com>
2021-08-25 16:24:32 +0800

    none

---
+++ _en/Computer/Data_Structure/Queue/Stack_Queue.cpp
@@ -0,0 +1,126 @@
+/* Stack_Queue.cpp
+Author: BSS9395
+Update: 2020-08-25T11:10:00+08@China-Guangdong-Zhanjiang+08
+Design: Queue Implementation by Two Stack
+Original: https://blog.csdn.net/ailunlee/article/details/85100514
+*/
+
+/* Stack Queue Model
+                                      Queue Front = x_0
+                                      Queue Back  = x_N
+Stack_In :  [x_0, x_1, ..., x_n]  =>  [x_{n+1}, x_{n+2}, ..., x_N]
+Stack_Out:  [                  ]  =>  [x_n, x_{n-1},     ..., x_0]
+*/
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+template<typename T>
+class Queue {
+private:
+    stack<T> _stack_in;
+    stack<T> _stack_out;
+
+public:
+    Queue() {
+
+    }
+
+public:
+    int size() {
+        return (_stack_in.size() + _stack_out.size());
+    }
+
+
+    bool empty() {
+        return (_stack_in.empty() && _stack_out.empty());
+    }
+
+
+    void push(T elem) {
+        _stack_in.push(elem);
+    }
+
+
+    T pop() {
+        T ret;
+        if (!_stack_out.empty()) {
+            ret = _stack_out.top();
+            _stack_out.pop();
+        }
+        else if (!_stack_in.empty()) {
+            while (!_stack_in.empty()) {
+                _stack_out.push(_stack_in.top());
+                _stack_in.pop();
+            }
+            ret = _stack_out.top();
+            _stack_out.pop();
+        }
+        else {
+            throw exception("The queue is empty.");
+        }
+        return ret;
+    }
+
+
+    T front() {
+        if (!_stack_out.empty()) {
+            return _stack_out.top();
+        }
+        else if (!_stack_in.empty()) {
+            while (!_stack_in.empty()) {
+                _stack_out.push(_stack_in.top());
+                _stack_in.pop();
+            }
+            return _stack_out.top();
+        }
+        else {
+            throw exception("The queue is empty.");
+        }
+    }
+
+
+    T back() {
+        if (!_stack_in.empty()) {
+            return _stack_in.top();
+        }
+        else if (!_stack_out.empty()) {
+            while (!_stack_out.empty()) {
+                _stack_in.push(_stack_out.top());
+                _stack_out.pop();
+            }
+            return _stack_in.top();
+        }
+        else {
+            throw exception("The queue is empty.");
+        }
+    }
+};
+
+
+int main(int argc, char *argv[]) {
+    Queue<int> queue = Queue<int>();
+    queue.push(9);
+    queue.push(0);
+    queue.push(8);
+    queue.push(2);
+    queue.push(6);
+
+    cout << "queue.front() = " << queue.front() << endl;
+    cout << "queue.back() = " << queue.back() << endl;
+    cout << "queue.size() = " << queue.size() << endl;
+    cout << "----------------------------------------" << endl;
+
+    queue.pop();
+    queue.pop();
+
+    cout << "queue.front() = " << queue.front() << endl;
+    cout << "queue.back() = " << queue.back() << endl;
+    cout << "queue.size() = " << queue.size() << endl;
+    cout << "----------------------------------------" << endl;
+
+    return 0;
+}
---
+++ _en/Computer/Data_Structure/Stack/Queue_Stack.cpp
@@ -0,0 +1,114 @@
+/* Queue_Stack.cpp
+Author: BSS9395
+Update: 2020-08-25T15:20:00+08@China-Guangdong-Zhanjiang+08
+Design: Stack Implementation by Two Queue
+Original: https://leetcode-cn.com/problems/implement-stack-using-queues/solution/yong-liang-ge-dui-lie-shi-xian-yi-ge-zhan-by-labyr/
+*/
+
+/* Queue Stack Model
+                                   Stack Top = x_n              Stack Top = x_{n-1}
+Queue1:  [x_0, x_1, ..., x_n]  =>  [x_n                   ]  => [x_0, x_1, ..., x_{n-2}]
+Queue2:  [                  ]  =>  [x_0, x_1, ..., x_{n-1}]  => [x_{n-1}               ]
+*/
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+template<typename T>
+class Stack {
+private:
+    queue<T> _queue1;
+    queue<T> _queue2;
+
+public:
+    Stack() {
+
+    }
+
+public:
+    int size() {
+        return (_queue1.size() + _queue2.size());
+    }
+
+
+    bool empty() {
+        return (_queue1.empty() && _queue2.empty());
+    }
+
+
+    void push(T elem) {
+        if (!_queue1.empty()) {
+            _queue1.push(elem);
+        }
+        else {
+            _queue2.push(elem);
+        }
+    }
+
+
+    T pop() {
+        T ret;
+        if (!_queue1.empty()) {
+            for (int size = _queue1.size(); 1 < size; size -= 1) {
+                _queue2.push(_queue1.front());
+                _queue1.pop();
+            }
+            ret = _queue1.front();
+            _queue1.pop();
+        }
+        else if (!_queue2.empty()) {
+            for (int size = _queue2.size(); 1 < size; size -= 1) {
+                _queue1.push(_queue2.front());
+                _queue2.pop();
+            }
+            ret = _queue2.front();
+            _queue2.pop();
+        }
+        else {
+            throw exception("The stack is empty");
+        }
+
+        return ret;
+    }
+
+
+    T top() {
+        T ret;
+        if (!_queue1.empty()) {
+            ret = _queue1.back();
+        }
+        else if (!_queue2.empty()) {
+            ret = _queue2.back();
+        }
+        else {
+            throw exception("The stack is empty");
+        }
+        return ret;
+    }
+
+};
+
+
+int main(int argc, char *argv[]) {
+    Stack<int> stack = Stack<int>();
+    stack.push(9);
+    stack.push(0);
+    stack.push(8);
+    stack.push(2);
+    stack.push(6);
+
+    cout << "stack.top() = " << stack.top() << endl;
+    cout << "stack.size() = " << stack.size() << endl;
+    cout << "----------------------------------------" << endl;
+
+    stack.pop();
+    stack.pop();
+
+    cout << "stack.top() = " << stack.top() << endl;
+    cout << "stack.size() = " << stack.size() << endl;
+    cout << "----------------------------------------" << endl;
+
+    return 0;
+}
