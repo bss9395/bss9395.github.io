6ade55e2aae5f8207c6e661fb89a050e883d46d2
bss9395 <brilliantstarrysky9395@gmail.com>
2021-10-07 21:20:29 +0800

    none

---
+++ _en/Computer/Operating_System/Java_Standard_Library/Loop_Label.java
@@ -0,0 +1,31 @@
+/* Loop_Label.java
+Author: BSS9395
+Update: 2021-10-07T20:57:00+08@China-Guangdong-Shenzhen+08
+Design: Java Loop Label
+*/
+
+public class Loop_Label {
+    public static void main(String[] args) {
+        Break:
+        for (int i = 0; i < 3; i += 1) {
+            for (int j = 0; j < 2; j += 1) {
+                if (i == 1) {
+                    break Break;
+                }
+                System.out.printf("[i, j] = [%d, %d] %n", i, j);
+            }
+        }
+
+        ////////////////////////////////
+        System.out.println("========================================");
+        Continue:
+        for (int i = 0; i < 3; i += 1) {
+            for (int j = 0; j < 2; j += 1) {
+                if (i == 1) {
+                    continue Continue;
+                }
+                System.out.printf("[i, j] = [%d, %d] %n", i, j);
+            }
+        }
+    }
+}
