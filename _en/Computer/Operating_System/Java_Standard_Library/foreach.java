4d043e0e6b3b2ca933523dc8faa5c54b0b510de7
bss9395 <brilliantstarrysky9395@gmail.com>
2021-10-10 21:59:06 +0800

    none

---
+++ _en/Computer/Operating_System/Java_Standard_Library/foreach.java
@@ -0,0 +1,31 @@
+/* foreach.java
+Author: BSS9395
+Update: 2021-10-10T21:31:00+08@China-Guangdong-Shenzhen+08
+Design: Java foreach Loop
+*/
+
+public class foreach {
+    public static void main(String[] args) {
+        String[] strings = new String[]{
+                "Java SE",
+                "Java EE",
+                "Java ME"
+        };
+
+        for (var string : strings) {
+            string = "Java";            // Original value will not change.
+            System.out.println(string);
+        }
+
+        System.out.println("========================================");
+        for (int i = 0; i < strings.length; i += 1) {
+            System.out.println(strings[i]);
+            strings[i] = "Java";
+        }
+
+        System.out.println("========================================");
+        for (var string : strings) {
+            System.out.println(string);
+        }
+    }
+}
