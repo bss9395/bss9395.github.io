3362ffef6ca785a34c0314c1fe73f815690f1d38
bss9395 <brilliantstarrysky9395@gmail.com>
2021-11-03 23:05:41 +0800

    none

---
+++ _en/Computer/Operating_System/C&C++_Standard_Library/#error.c
@@ -0,0 +1,18 @@
+/* #error.c
+Author: BSS9395
+Update: 2021-11-03T23:03:00+08@China-Guangdong-Shenzhen+08
+Design: C Standard Directive: #error
+*/
+
+#ifndef _WIN32
+#error "Target Windows 32bit or 64bit version."
+#endif // _WIN32
+
+#ifndef _WIN64
+#error "Target Windows 64bit version."
+#endif // _WIN64
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
