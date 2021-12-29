c2283fc925e90e885b7b4441cc8e83f3ae9b6181
bss9395 <brilliantstarrysky9395@gmail.com>
2021-12-29 22:49:17 +0800

    none

--- _en/Computer/Operating_System/OpenGL_C_Programming/X-Z_Coordinate.cpp
+++ _en/Computer/Operating_System/OpenGL_C_Programming/X-Z_Coordinate.cpp
@@ -1,6 +1,6 @@
 /* X-Z_Coordinate.cpp
 Author: BSS9395
-Update: 2021-12-29T18:23:00+08@China-Guangdong-Shenzhen+08
+Update: 2021-12-29T22:47:00+08@China-Guangdong-Shenzhen+08
 Design: X-Z Coordinate
 */
 
@@ -21,16 +21,19 @@ Design: X-Z Coordinate
 ////////////////////////////////////////////////////////////////////////////////
 
 std::string _Buffer = std::string(1024, '\0');
-static int _Width_sc = 800;
-static int _Height_sc = 600;
+int _Width_sc = 800;
+int _Height_sc = 600;
 GLFWwindow *_Window = NULL;
-unsigned int _VAO = 0;
-unsigned int _VBO = 0;
 unsigned int _Texture = 0;
-unsigned int _Program = 0;
+unsigned int _VAO_Cube = 0;
+unsigned int _VBO_Cube = 0;
+unsigned int _VAO_Axis = 0;
+unsigned int _VBO_Axis = 0;
+unsigned int _PS_Cube = 0;
+unsigned int _PS_Axis = 0;
 
-float _Vertices[5 * 24] = {
-    // ![Cube_Order.svg](figures/Cube_Order.svg)
+// ![Cube_Order.svg](figures/Cube_Order.svg)
+float _Cube[5 * 24] = {
     // X-Z coordinate
     // -X <= -Y <= -Z <= +Z <= +Y <= +X
     // +X, CCW => CW, upward
@@ -65,17 +68,25 @@ float _Vertices[5 * 24] = {
     -0.5, -0.5, +0.5, 1.0, 1.0
 };
 
-const char *_Vertex_Shader = R"(
+float _Axis[6 * 3] = {
+    // X-Z coordinate
+    // +X
+    +0.0, +0.0, +0.0, +1.0, +0.0, +0.0,
+    // +Y
+    +0.0, +0.0, +0.0, +0.0, +1.0, +0.0,
+    // +Z
+    +0.0, +0.0, +0.0, +0.0, +0.0, +1.0
+};
+
+const char *_VS = R"(
 /* @Shader: Vertex */
 #version 330 core
 
 uniform mat4 _Model;
 uniform mat4 _View;
 uniform mat4 _Projection;
-
 layout(location = 0) in vec3 _vs_Position;
 layout(location = 1) in vec2 _vs_Mapping;
-
 out vec2 _fs_Mapping;
 
 void main() {
@@ -84,18 +95,27 @@ void main() {
 }
 )";
 
-const char *_Fragment_Shader = R"(
+const char *_FS_Cube = R"(
 /* @Shader: Fragment */
 #version 330 core
 
 uniform sampler2D _Texture;
-
 in vec2 _fs_Mapping;
-
 out vec4 _fs_Color;
 
 void main() {
     _fs_Color = texture(_Texture, _fs_Mapping);
+}
+)";
+
+const char *_FS_Axis = R"(
+/* @Shader: Fragment */
+#version 330 core
+
+out vec4 _fs_Color;
+
+void main() {
+    _fs_Color = vec4(0.0, 1.0, 0.0, 1.0);  // color green
 }
 )";
 
@@ -126,11 +146,11 @@ int main(int argc, char *argv[]) {
 
     ////////////////////////////////////
 
-    glGenVertexArrays(1, &_VAO);
-    glGenBuffers(1, &_VBO);
-    glBindVertexArray(_VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, _VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(_Vertices), _Vertices, GL_STATIC_DRAW);
+    glGenVertexArrays(1, &_VAO_Cube);
+    glGenBuffers(1, &_VBO_Cube);
+    glBindVertexArray(_VAO_Cube);
+    glBindBuffer(GL_ARRAY_BUFFER, _VBO_Cube);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(_Cube), _Cube, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
@@ -161,74 +181,127 @@ int main(int argc, char *argv[]) {
     }
     stbi_image_free(data);
 
+    glGenVertexArrays(1, &_VAO_Axis);
+    glGenBuffers(1, &_VBO_Axis);
+    glBindVertexArray(_VAO_Axis);
+    glBindBuffer(GL_ARRAY_BUFFER, _VBO_Axis);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(_Axis), _Axis, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     ////////////////////////////////////
 
-    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &_Vertex_Shader, NULL);
-    glCompileShader(vertex_shader);
-    unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &_Fragment_Shader, NULL);
-    glCompileShader(fragment_shader);
-    _Program = glCreateProgram();
-    glAttachShader(_Program, vertex_shader);
-    glAttachShader(_Program, fragment_shader);
-    glLinkProgram(_Program);
     int success = 0;
-    glGetProgramiv(_Program, GL_LINK_STATUS, &success);
+    unsigned int vs_cube = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs_cube, 1, &_VS, NULL);
+    glCompileShader(vs_cube);
+
+    unsigned int fs_cube = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs_cube, 1, &_FS_Cube, NULL);
+    glCompileShader(fs_cube);
+    _PS_Cube = glCreateProgram();
+    glAttachShader(_PS_Cube, vs_cube);
+    glAttachShader(_PS_Cube, fs_cube);
+    glLinkProgram(_PS_Cube);
+    glGetProgramiv(_PS_Cube, GL_LINK_STATUS, &success);
     if (success == 0) {
-        glGetProgramInfoLog(_Program, (GLsizei)_Buffer.size(), NULL, _Buffer.data());
+        glGetProgramInfoLog(_PS_Cube, (GLsizei)_Buffer.size(), NULL, _Buffer.data());
         fprintf(stderr, "%s""\n", _Buffer.data());
         glfwTerminate();
     }
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
+
+    unsigned int fs_axis = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs_axis, 1, &_FS_Axis, NULL);
+    glCompileShader(fs_axis);
+    _PS_Axis = glCreateProgram();
+    glAttachShader(_PS_Axis, vs_cube);
+    glAttachShader(_PS_Axis, fs_axis);
+    glLinkProgram(_PS_Axis);
+    glGetProgramiv(_PS_Axis, GL_LINK_STATUS, &success);
+    if (success == 0) {
+        glGetProgramInfoLog(_PS_Axis, (GLsizei)_Buffer.size(), NULL, _Buffer.data());
+        fprintf(stderr, "%s""\n", _Buffer.data());
+        glfwTerminate();
+    }
+
+    glDeleteShader(vs_cube);
+    glDeleteShader(fs_cube);
+    glDeleteShader(fs_axis);
 
     ////////////////////////////////////
 
+    glEnable(GL_LINE_SMOOTH);
     glEnable(GL_MULTISAMPLE);
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_CULL_FACE);
     glFrontFace(GL_CCW);
     glCullFace(GL_BACK);
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    glUseProgram(_Program);
-    glUniform1i(glGetUniformLocation(_Program, "_Texture"), 0);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, _Texture);
+    glLineWidth(5.0f); // maybe unsupported.
+    GLfloat range[2] = { 0.0f, 0.0f };
+    glGetFloatv(GL_ALIASED_LINE_WIDTH_RANGE, range);
+    fprintf(stderr, "%f, %f \n", range[0], range[1]);
+    glGetFloatv(GL_LINE_WIDTH_GRANULARITY, range);
+    fprintf(stderr, "%f, %f""\n", range[0], range[1]);
+    glGetFloatv(GL_LINE_WIDTH, range);
+    fprintf(stderr, "%f, %f""\n", range[0], range[1]);
+
+    ////////////////////////////////////
 
     // X-Y coordinate <= X-Z coordinate.
-    glm::mat4 coord = glm::rotate(glm::mat4(1.0), glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    glm::mat3 coordinate = glm::rotate(glm::mat4(1.0), glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
     while (!glfwWindowShouldClose(_Window)) {
         On_Key(_Window);
 
         glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = coord * model;                              // X-Y coordinate <= X-Z coordinate.
+        glm::mat4 model = coordinate;                                // X-Y coordinate <= X-Z coordinate.
         model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0)); // rotate around +Z axis
 
-        glm::vec3 camera = glm::vec3(+2.0f, +2.0f, -2.0f);  // camera in X-Y coordinate
-        glm::vec3 pole = glm::vec3(0.0, 1.0, 0.0);
-        glm::vec3 right = glm::cross(pole, camera);
-        glm::vec3 up = glm::cross(camera, right);
-        glm::mat4 view = glm::lookAt(camera, glm::vec3(0.0f, 0.0f, 0.0f), glm::normalize(up));
+        glm::vec3 eye = coordinate * glm::vec3(+2.0f, +2.0f, +2.0f); // camera in X-Z coordinate.
+        glm::vec3 center = coordinate * glm::vec3(0.0f, 0.0f, 0.0f); // object in X-Z coordinate.
+        glm::vec3 pole = coordinate * glm::vec3(0.0f, 0.0f, 1.0f);   // pole in X-Z coordinate.
+        glm::vec3 right = glm::cross(center - eye, pole);
+        glm::vec3 up = glm::cross(right, center - eye);
+        glm::mat4 view = glm::lookAt(eye, center, up);
 
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)_Width_sc / (float)_Height_sc, 0.1f, 100.0f);
-        glUniformMatrix4fv(glGetUniformLocation(_Program, "_Model"), 1, GL_FALSE, &model[0][0]);
-        glUniformMatrix4fv(glGetUniformLocation(_Program, "_View"), 1, GL_FALSE, &view[0][0]);
-        glUniformMatrix4fv(glGetUniformLocation(_Program, "_Projection"), 1, GL_FALSE, &projection[0][0]);
 
-        glBindVertexArray(_VAO);
+        glUseProgram(_PS_Cube);
+        glUniformMatrix4fv(glGetUniformLocation(_PS_Cube, "_Model"), 1, GL_FALSE, &model[0][0]);
+        glUniformMatrix4fv(glGetUniformLocation(_PS_Cube, "_View"), 1, GL_FALSE, &view[0][0]);
+        glUniformMatrix4fv(glGetUniformLocation(_PS_Cube, "_Projection"), 1, GL_FALSE, &projection[0][0]);
+        glUniform1i(glGetUniformLocation(_PS_Cube, "_Texture"), 0);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, _Texture);
+        glBindVertexArray(_VAO_Cube);
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 24);
+
+        ////////////////////////////////
+
+        glUseProgram(_PS_Axis);
+        model = coordinate;
+        glUniformMatrix4fv(glGetUniformLocation(_PS_Axis, "_Model"), 1, GL_FALSE, &model[0][0]);
+        glUniformMatrix4fv(glGetUniformLocation(_PS_Axis, "_View"), 1, GL_FALSE, &view[0][0]);
+        glUniformMatrix4fv(glGetUniformLocation(_PS_Axis, "_Projection"), 1, GL_FALSE, &projection[0][0]);
+
+        glBindVertexArray(_VAO_Axis);
+        glDrawArrays(GL_LINES, 0, 6);
 
         glfwSwapBuffers(_Window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &_VAO);
-    glDeleteBuffers(1, &_VBO);
-    glDeleteProgram(_Program);
+    glDeleteVertexArrays(1, &_VAO_Cube);
+    glDeleteVertexArrays(1, &_VAO_Axis);
+    glDeleteBuffers(1, &_VBO_Cube);
+    glDeleteBuffers(1, &_VBO_Axis);
+    glDeleteProgram(_PS_Cube);
+    glDeleteProgram(_PS_Axis);
     glfwDestroyWindow(_Window);
     glfwTerminate();
     return 0;
