b801c64109aebe9ac8f93c2c9b4e534d6df67713
bss9395 <brilliantstarrysky9395@gmail.com>
2022-01-09 23:12:51 +0800

    none

---
+++ _en/Computer/Operating_System/C&C++_Standard_Library/typeid.cpp
@@ -0,0 +1,81 @@
+/* typeid.cpp
+Author: bss9395
+Update: 2022-01-09T23:08:00+08@China-Guangdong-Shenzhen+08
+Design: C++ Standard Keyword: typeid
+Encode: UTF-8
+*/
+
+/*
+#include <typeinfo>
+std::type_info::name;
+const char* name() const noexcept;
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <functional>
+#include <typeinfo>
+
+typedef intptr_t iptr;
+typedef std::string String;
+typedef const char *Level;
+
+template<typename TRecord = String, typename TSolution = String>
+class Exception : public std::exception {
+public:
+    Level _level = Level();
+    String _file = String();
+    iptr _line = 0;
+    String _function = String();
+    TRecord _record = TRecord();
+    TSolution _solution = TSolution();
+    String _report = String(512, 0);
+
+public:
+    Exception(Level level, const String &file, iptr line, const String &function, const TRecord &record, const TSolution &solution = TSolution())
+        : _level(level), _file(file), _line(line), _function(function), _record(record), _solution(solution) {
+        // fprintf(stderr, "[%td] %s"";\n", (iptr)__LINE__, __FUNCTION__); fflush(stderr);
+
+        fprintf(stderr, "[%td] typeid(record).name() = %s""\n", (iptr)__LINE__, typeid(record).name());
+    }
+
+    Exception(Level level, const String &file, iptr line, const String &function, const char *record, const char *solution = "")
+        : _level(level), _file(file), _line(line), _function(function), _record(record), _solution(solution) {
+        // fprintf(stderr, "[%td] %s"";\n", (iptr)__LINE__, __FUNCTION__); fflush(stderr);
+
+        fprintf(stderr, "[%td] typeid(record).name() = %s""\n", (iptr)__LINE__, typeid(record).name());
+    }
+
+    virtual ~Exception() {
+        // fprintf(stderr, "[%td] %s"";\n", (iptr)__LINE__, __FUNCTION__); fflush(stderr);
+    }
+
+public:
+    virtual String &Report() {
+        iptr numb = snprintf(&_report[0], _report.capacity(), "[%s : %s : %td : %s] ", _level, &_file[(iptr)(_file.find_last_of("/\\")) + 1], _line, &_function[0]);
+        _report.resize(numb);
+        _report += _record, _report += " : ";
+        _report += _solution, _report += ";";
+        return _report;
+    }
+
+    virtual const char *what() const noexcept override {
+        (*(Exception *)this).Report(); // discard const qualifier.
+        return &_report[0];
+    }
+};
+
+
+int main(int argc, char *argv[]) {
+    try {
+        if (true) {
+            throw Exception("Debug", __FILE__, __LINE__, __FUNCTION__, "record", "solution");
+        }
+    }
+    catch (std::exception &except) {
+        fprintf(stderr, "%s""\n", except.what());
+    }
+
+    return 0;
+}
