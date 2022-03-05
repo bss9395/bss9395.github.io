c0a687cb8871206cbefe61b51a1e247f65361373
bss9395 <brilliantstarrysky9395@gmail.com>
2022-03-06 00:11:38 +0800

    none

---
+++ _en/Computer/Data_Structure/Tree/Trie_Tree.cpp
@@ -0,0 +1,124 @@
+/* Trie_Tree.cpp
+Author: BSS9395
+Update: 2022-03-06T00:07:00+08@China-Guangdong-Zhanjiang+08
+Design: Trie tree
+Encode: UTF-8
+*/
+
+#include <iostream>
+#include <string>
+#include <functional>
+typedef intptr_t iptr;
+
+struct Trie {
+    struct Node {
+        Node* _next[26];
+        iptr _leaf = 0;
+
+    public:
+        explicit Node() {
+            fprintf(stderr, "[%td] %s""\n", (iptr)__LINE__, __FUNCTION__);
+
+            for (iptr i = 0; i < 26; i += 1) {
+                _next[i] = nullptr;
+            }
+        }
+
+        virtual ~Node() {
+            fprintf(stderr, "[%td] %s""\n", (iptr)__LINE__, __FUNCTION__);
+        }
+    };
+
+public:
+    Node _head = Node();
+
+public:
+    explicit Trie() {
+        fprintf(stderr, "[%td] %s""\n", (iptr)__LINE__, __FUNCTION__);
+    }
+
+    virtual ~Trie() {
+        fprintf(stderr, "[%td] %s""\n", (iptr)__LINE__, __FUNCTION__);
+    }
+
+public:
+    void Print() {
+        fprintf(stderr, "[%td] %s""\n", (iptr)__LINE__, __FUNCTION__);
+
+        typedef std::function<void(Node*, iptr)> Function;
+        Function Traverse = [&Traverse](Node* node, iptr level)  -> void {
+            fprintf(stderr, "[%td] %s""\n", (iptr)__LINE__, "Function Traverse = [&Traverse](Node* node)  -> void {");
+
+            if (node == nullptr) {
+                return;
+            }
+            for (iptr i = 0; i < 26; i += 1) {
+                if (node->_next[i] != nullptr) {
+                    fprintf(stderr, "[%td] %c", level, (char)(i + 'a'));
+                    fflush(stderr);
+                    Traverse(node->_next[i], level + 1);
+                }
+            }
+        };
+        Traverse(&_head, 0 + 1);
+    }
+
+    void Insert(const std::string& word) {
+        fprintf(stderr, "[%td] %s""\n", (iptr)__LINE__, __FUNCTION__);
+
+        Node* node = &_head;
+        const char* chara = word.data();
+        for (iptr i = 0, leng = word.length(); i < leng; i += 1) {
+            if (node->_next[chara[i] - 'a'] == nullptr) {
+                node->_next[chara[i] - 'a'] = new Node();
+                node->_leaf += 1;
+            }
+            node = node->_next[chara[i] - 'a'];
+        }
+    }
+
+    bool Prifix(const std::string& prefix) {
+        fprintf(stderr, "[%td] %s""\n", (iptr)__LINE__, __FUNCTION__);
+
+        Node* node = &_head;
+        const char* chara = prefix.data();
+        for (iptr i = 0, leng = prefix.length(); i < leng; i += 1) {
+            node = node->_next[chara[i] - 'a'];
+            if (node == nullptr) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool Search(const std::string& word) {
+        fprintf(stderr, "[%td] %s""\n", (iptr)__LINE__, __FUNCTION__);
+
+        Node* node = &_head;
+        const char* chara = word.data();
+        for (iptr i = 0, leng = word.length(); i < leng; i += 1) {
+            node = node->_next[chara[i] - 'a'];
+            if (node == nullptr) {
+                return false;
+            }
+        }
+        return (node->_leaf <= 0);
+    }
+};
+
+
+void Test() {
+    Trie trie = Trie();
+    trie.Insert("word");
+    trie.Print();
+    bool prifix = trie.Prifix("word");
+    std::cout << std::boolalpha << prifix << std::endl;
+    bool search = trie.Search("wo");
+    std::cout << std::boolalpha << search << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    Test();
+
+    return 0;
+}
